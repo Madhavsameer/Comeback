eafee1d0d37de2ac0b5de04ae42464b9f079513b
Madhav sameer <madhavsameer97@gmail.com>
2024-12-30 09:28:33 +0530

    my mess

---
+++ JAVA+DSA/basics.java
@@ -0,0 +1,10 @@
+public class basics {
+
+    public static void main(String[] args) {
+        greet("Madhav");
+    }
+
+    public static void greet(String name){
+        System.out.println("Hello "+name);
+    }
+}
